491bcf42667e91f1d47b3c17b9913aaae7875a5b
Daniel Castro <c11daniel.castro@gmail.com>
2022-01-03 11:22:55 +0000

    Added documentation and datasets

---
+++ datasets/raw-dataset-treament/extract_id_dataset.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+df = pd.read_excel("só barroso.xlsx", header=[0])
+
+ids = []
+for url in df['source page']:
+    url = url.rsplit('/', 1)[-1]
+    ids.append(url)
+
+df['id'] = ids
+
+for desc in df['Descrição']:
+    print(desc)
+
+
+# df.to_excel("dataset.xlsx")
